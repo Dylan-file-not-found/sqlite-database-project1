e44df6686ce3099da8fd415e8307ff2b28472122
Dylan-file-not-found <155810498+Dylan-file-not-found@users.noreply.github.com>
2024-07-24 22:04:17 +0300

    Delete Projectsql.py

--- Projectsql.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import panda as pd
-# Provide the correct path to your SQLite database file
-df = pd.read.csv('data.csv')
-
-df.columns = df.columns.str.strip()
-connection = sqlite3.connect('SqliteDBproject.db')
-df.to_sql('United_State',connection ,if_exists = "replace")
-connection.close()
